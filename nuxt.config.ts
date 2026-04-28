8e7a2f7ef9911dd72c5225a087c2484e4a0ec63d
Namratachougule3792 <namratachougule3792@gmail.com>
2026-04-28 16:15:05 +0530

    fix amplify config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,20 @@ export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
 
   runtimeConfig: {
+    // server side (IMPORTANT)
     supabaseUrl: process.env.SUPABASE_URL,
     supabaseKey: process.env.SUPABASE_KEY,
 
     public: {
+      // client side
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseKey: process.env.SUPABASE_KEY,
       dummyApi: process.env.DUMMY_API
     }
+  },
+
+  //  MOST IMPORTANT FOR AMPLIFY
+  nitro: {
+    preset: 'aws_amplify'
   }
 })
